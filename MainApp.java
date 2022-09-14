c05b4cc8fadc496f1baa500146c7d82583626914
2261-JOEL <113163675+2261-JOEL@users.noreply.github.com>
2022-09-14 11:24:51 +0530

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,18 @@
+
+public class MainApp {
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		Student s2 = new Student();
+		
+		s1.name="Joel";
+		s1.rollno=123;
+		s1.regno=456;
+		s1.admission();
+		
+		s2.name="Nagesh";
+		s2.rollno=456;
+		s2.regno=789;
+		s2.admission();
+	}
+
+}
---
+++ Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+	Integer rollno, regno;
+	String name;
+	
+	void admission() {
+		System.out.println(name+" has taken admission. "
+				+ "The assigned roll number is " +rollno+" "
+						+ "and the registration no. is  "+regno);
+//		System.out.println("The assigned roll no. is ");
+//		System.out.println("Registraion no is "+regno);
+	}
+
+}
